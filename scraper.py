5f24059cb3e70ec1b7b38dbb4dcc5a0c9b553be0
loic.macherel <loic.macherel@gmail.com>
2020-12-04 17:13:42 +0100

    Create live scraping of data

---
+++ scraper.py
@@ -0,0 +1,24 @@
+import logging
+
+import arrow
+import requests
+
+logger = logging.getLogger(__name__)
+
+API_URL = "https://opendata.reseaux-energies.fr/api/records/1.0/search/"
+
+LIVE_DATASET = "eco2mix-regional-tr"
+CONSOLIDATED_DATASET = "eco2mix-regional-cons-def"
+
+
+def query_regional_production_live(reference_datetime: arrow.Arrow):
+    """Query regional data on production on the past day. This will create duplicates."""
+    params = {"dataset": LIVE_DATASET, "q": get_datetime_query_param(reference_datetime, interval_days=1)}
+    res = requests.get(API_URL, params=params)
+    return res.json()
+
+
+def get_datetime_query_param(end_datetime: arrow.Arrow, interval_days:int=1) -> str:
+    start = end_datetime.shift(days=-interval_days).format("YYYY-MM-DDTHH:mm")
+    end = end_datetime.format("YYYY-MM-DDTHH:mm")
+    return f"date_heure:[{start} TO {end}]"
