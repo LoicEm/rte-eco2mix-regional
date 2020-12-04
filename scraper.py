c7efd89f8e031cffb11e9cd7fdd394cae35c27c1
loic.macherel <loic.macherel@gmail.com>
2020-12-04 17:21:01 +0100

    Add a line of logging

--- scraper.py
+++ scraper.py
@@ -12,13 +12,18 @@ CONSOLIDATED_DATASET = "eco2mix-regional-cons-def"
 
 
 def query_regional_production_live(reference_datetime: arrow.Arrow):
-    """Query regional data on production on the past day. This will create duplicates."""
-    params = {"dataset": LIVE_DATASET, "q": get_datetime_query_param(reference_datetime, interval_days=1)}
+    """Query regional data on production on the past day.
+    This will create duplicates."""
+    logger.debug(f"querying live data for {reference_datetime} ")
+    params = {
+        "dataset": LIVE_DATASET,
+        "q": get_datetime_query_param(reference_datetime, interval_days=1),
+    }
     res = requests.get(API_URL, params=params)
     return res.json()
 
 
-def get_datetime_query_param(end_datetime: arrow.Arrow, interval_days:int=1) -> str:
+def get_datetime_query_param(end_datetime: arrow.Arrow, interval_days: int = 1) -> str:
     start = end_datetime.shift(days=-interval_days).format("YYYY-MM-DDTHH:mm")
     end = end_datetime.format("YYYY-MM-DDTHH:mm")
     return f"date_heure:[{start} TO {end}]"
