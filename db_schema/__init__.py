e0ca4601f972437ba2466b6c9a360c2f06fa6c6e
Loic Macherel <loic.macherel@gmail.com>
2021-01-25 13:28:20 +0100

    Remove ORM references and migrations

--- db_schema/__init__.py
+++
@@ -1,4 +0,0 @@
-from db_schema.regional_production import RegionalEnergyProduction
-
-
-__all__ = [RegionalEnergyProduction]
--- db_schema/base.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy_repr import RepresentableBase
-
-Base = declarative_base(cls=RepresentableBase)
